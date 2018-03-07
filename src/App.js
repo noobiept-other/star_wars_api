fe1d5c6e402b533e572d1708e54cd4d5882c1a54
Pedro Ferreira <plferreira4@gmail.com>
2018-03-07 14:50:37 +0000

    Do a simple query to the API and show a list of people's names.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  render() {
-    return (
-      <div>Hello</div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            people: []
+        };
+    }
+
+    async componentDidMount() {
+        let data = await this.getPeople();
+        let names = data.results.map( (element) => element.name );
+
+        this.setState({
+            people: names
+        })
+    }
+
+    async getPeople( page= 1 ) {
+        const url = `https://swapi.co/api/people/?page=${ page }`;
+        let response = await fetch( url );
+        let data = await response.json();
+
+        return data;
+    }
+
+    render() {
+        let peopleList = this.state.people.map(
+            (element, index) => <li key={ index }>{ element }</li>
+        );
+
+        return (
+            <div>
+                <div>People</div>
+                <ul>{ peopleList }</ul>
+            </div>
+        );
+    }
 }
 
 export default App;
